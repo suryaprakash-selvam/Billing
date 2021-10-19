d62fb4f65b4a36823cd107320752be702d219ad7
Suryaprakash Selvam <selvamssp2004@gmail.com>
2021-10-19 11:07:15 +0530

    msbs changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NewproductComponent } from './newproduct/newproduct.component';
+
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import {ButtonModule} from 'primeng/button';
 import {TableModule} from 'primeng/table';
 import { FormsModule }   from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {ScrollPanelModule} from 'primeng/scrollpanel';
-import { BillingComponent } from './billing/billing.component';
 import {ConfirmDialogModule} from 'primeng/confirmdialog';
 import {ConfirmationService} from 'primeng/api';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -22,7 +21,6 @@ import {InputSwitchModule} from 'primeng/inputswitch';
 import {RadioButtonModule} from 'primeng/radiobutton';
 import {InputNumberModule} from 'primeng/inputnumber';
 import {DialogModule} from 'primeng/dialog';
-import { UpdateAvailabilityComponent } from './update-availability/update-availability.component';
 import { StatusComponent } from './status/status.component';
 import {InputMaskModule} from 'primeng/inputmask';
 
@@ -30,9 +28,6 @@ import {InputTextareaModule} from 'primeng/inputtextarea';
 @NgModule({
   declarations: [
     AppComponent,
-    NewproductComponent,
-    BillingComponent,
-    UpdateAvailabilityComponent,
     StatusComponent
   ],
   schemas: [
